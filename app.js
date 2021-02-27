83120c478253678f7a29bf007130a8d879dd5ea3
Olga Konopleva <olenenok.she@gmail.com>
2021-02-27 22:27:48 +0200

    добавлен рендер

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exhbs = require('express-handlebars');
+const products = require('./products.json')
 
 const app = express()
 app.set('view engine', 'hbs');
@@ -15,10 +16,21 @@ app.use(express.static('public'))
   })
 
   app.get('/', function (req, res) {
-    res.send('<h1>hello /</h1>');
+    res.render('home', {cssFileName: 'home', pageName: "Главная"});
+    // res.send('<h1>hello /</h1>');
     // console.log(req);
   })
 
   app.get('/about', function (req, res) {
-    res.send('hello about')
+    res.render('about', {cssFileName: 'products', pageName: "О нас"});
+  })
+
+  app.get('/products', function (req, res) {
+    res.render('products', {products, cssFileName: 'products', pageName: "Товары"});
+  })
+
+  app.get('/product/:productId', function(req,res) {
+    const product = products.find(p => p.id === req.params.productId);
+    console.log(product);
+    res.render('product', {product})
   })
